59e960f9ec2bd82e75810fa21851844ad108df0f
schm00g <hk8igureu@relay.firefox.com>
2022-05-06 20:34:03 +0100

    gs

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,33 @@
 import '../styles/globals.css'
+import '@rainbow-me/rainbowkit/styles.css';
+
+import {
+  apiProvider,
+  configureChains,
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+
+import { chain, createClient, WagmiProvider } from 'wagmi';
+
+...
+import { chain, createClient, WagmiProvider } from 'wagmi';
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
+  [
+    apiProvider.alchemy(process.env.ALCHEMY_ID),
+    apiProvider.fallback()
+  ]
+);
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  chains
+});
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+})
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
